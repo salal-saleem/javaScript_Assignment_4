5f0ba2e23441883512cdb51c8b8532b3bbf57407
Salal_Saleem <87649739+salal-saleem@users.noreply.github.com>
2024-03-18 10:57:09 +0500

    add files way upload

---
+++ chapter_14to16/app.js
@@ -0,0 +1,122 @@
+/// assignment 1
+
+// var studentName = [];
+
+
+// assignment 2
+
+// var studentName = {};
+
+
+// assignment 3
+
+// var names = ['ali','usman','kamran','salman'] ;
+
+// assignment 4
+
+// var numbers = [20,40,50,80,130,400];
+
+// assignment 5
+
+// var booleans = [true,false];
+
+// assignment 6
+
+// var mixArray = ['string',30,false];
+
+// assignment 7
+
+// var educationsInPakistan = ['SSC','HSC','BCS','BS','BCOM','MS','M. Phil.','PhD']
+
+// document.write(`Qualifications: <br><br> 1)${educationsInPakistan[0]} <br>2)${educationsInPakistan[1]}
+// <br>3)${educationsInPakistan[2]} <br>4)${educationsInPakistan[3]} <br>5)${educationsInPakistan[4]}<br>6)${educationsInPakistan[5]}
+// <br>7)${educationsInPakistan[6]}<br>8)${educationsInPakistan[7]}
+// `)
+
+
+// assignment 8
+
+// var studentName = ['micheal','john','tony'];
+// var studentMarks = [320,230,480];
+
+// document.write(`Score of ${studentName[0]} is ${studentMarks[0]}. Percentage: ${studentMarks[0]/500*100}%
+//     <br>Score of ${studentName[1]} is ${studentMarks[1]}. Percentage: ${studentMarks[1]/500*100}%
+//     <br>Score of ${studentName[2]} is ${studentMarks[2]}. Percentage: ${studentMarks[2]/500*100}%
+// `)
+
+
+// assignment 10
+
+// var scoreOfStudents = [320,230,480,120];
+// var scoreOfStudent = [320,230,480,120];
+// scoreOfStudent.sort()
+
+// document.write(`Score of Students :${scoreOfStudents}<br>Sorted Score of Students :${scoreOfStudent}`)
+
+
+// assignment 11
+
+// var cities = ['karachi','quetta','islamabad','lahore','peshawar']
+
+// document.write(`cities list: <br> ${cities} <br> Selected Cities List:${cities.splice(2,2)} `)
+
+// assignment 12
+
+// var arr = ['This','is', ' my', 'cat'];
+// var join = arr.join(' ');
+
+// document.write(`array : <br> ${arr}<br>
+// ${join}`)
+
+
+// assignment 13
+
+// var queue = [];
+// queue.push('keyboard')
+// queue.push('mouse')
+// queue.push('printer')
+// queue.push('moniter')
+
+// document.write(`devices : <br> ${queue}
+// <br><br>
+// out: <br> ${queue[0]}
+// <br>out: <br> ${queue[1]}
+// <br>out: <br> ${queue[2]}
+// <br>out: <br> ${queue[3]}
+
+// `)
+
+
+// assignment 14
+
+// var queue = [];
+// queue.push('keyboard')
+// queue.push('mouse')
+// queue.push('printer')
+// queue.push('moniter')
+
+// document.write(`devices : <br> ${queue}
+// <br><br>
+// out: <br> ${queue[3]}
+// <br>out: <br> ${queue[2]}
+// <br>out: <br> ${queue[1]}
+// <br>out: <br> ${queue[0]}
+
+// `)
+
+
+// var phones = ['samsung','iphone','nokia','sony','haier'];
+
+// document.write(`
+// select your favourite phone brand : <br>
+// <select name="one" id="">
+// <option value="" name="one">${phones[0]}</option>
+// <option value="" name="one">${phones[1]}</option>
+// <option value="" name="one">${phones[2]}</option>
+// <option value="" name="one">${phones[3]}</option>
+// <option value="" name="one">${phones[4]}</option>
+// </select>
+
+// `)
+
+
